199d15bdb40ebb137180e4fdfd6648e0d751bf59
unknown <hosam.mohamed@student.vinci.be>
2021-05-02 15:13:44 +0200

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,8 +11,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "1d62dcf38e1cb6205368ec3338f98b1d",
-"/": "1d62dcf38e1cb6205368ec3338f98b1d",
+"index.html": "35d9f6de4c0fb9cb53b337e24007f25a",
+"/": "35d9f6de4c0fb9cb53b337e24007f25a",
 "main.dart.js": "faa60ebedd71a4e57fe99d2ad7ded85c",
 "manifest.json": "ea00f66c0173ce3a786de93c76d3fc3e",
 "version.json": "2b9cd6f91a8662e5926af33d0b079325"
